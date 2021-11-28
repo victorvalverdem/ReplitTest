425c749944f485b44645613f35dd3fdc117ac7b8
V. M. Valverde <93843339+victorvalverdem@users.noreply.github.com>
2021-11-28 02:28:13 +0000

    Creación clase Batman y get de Anios (test)

---
+++ Batman.java
@@ -0,0 +1,15 @@
+public class Batman
+{
+  private int anios_siendo_Bats;
+  private int villanos_vencidos;
+
+  public Batman(int anios, int villanos)
+  {
+    this.anios_siendo_Bats = anios;
+    this.villanos_vencidos = villanos;
+  }
+  public int getAnios()
+  {
+    return anios_siendo_Bats;
+  }
+}
--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main
   {
     System.out.println("Hello world!");
     System.out.println("Finalmente se configuró esta verga");
+    Batman bat1 = new Batman(12, 1000);
+    
+    System.out.println(bat1.getAnios());
   }
 }
