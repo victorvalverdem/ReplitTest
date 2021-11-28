0100e9fcb1a1f6b9589089249f0eab7e24d0c8af
V. M. Valverde <93843339+victorvalverdem@users.noreply.github.com>
2021-11-28 02:51:40 +0000

    Eliminación atributo de villanos vencidos

--- Batman.java
+++ Batman.java
@@ -1,12 +1,10 @@
 public class Batman
 {
   private int anios_siendo_Bats;
-  private int villanos_vencidos;
 
   public Batman(int anios, int villanos)
   {
     this.anios_siendo_Bats = anios;
-    this.villanos_vencidos = villanos;
   }
   public int getAnios()
   {
